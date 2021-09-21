4c671d6a8a090663c44c94d2a90a3af3205ad77d
quangdq <quangdq@ftech.ai>
2021-09-21 23:28:35 +0700

    change color mode for visualiize

--- demo/predictor.py
+++ demo/predictor.py
@@ -17,7 +17,7 @@ from adet.utils.visualizer import TextVisualizer
 
 
 class VisualizationDemo(object):
-    def __init__(self, cfg, instance_mode=ColorMode.SEGMETATION, parallel=False):
+    def __init__(self, cfg, instance_mode=ColorMode.SEGMENTATION, parallel=False):
         """
         Args:
             cfg (CfgNode):
