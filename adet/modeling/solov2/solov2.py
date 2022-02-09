07b764171fd5be149de4c54ca1177d666f6007de
quangdq <quangdq@ftech.ai>
2022-02-09 09:25:47 +0700

    update learning rate

--- adet/modeling/solov2/solov2.py
+++ adet/modeling/solov2/solov2.py
@@ -494,12 +494,12 @@ class SOLOv2(nn.Module):
         results.pred_masks = seg_masks
 
         # get bbox from mask
-        pred_boxes = torch.zeros(seg_masks.size(0), 4)
-        for i in range(seg_masks.size(0)):
-           mask = seg_masks[i].squeeze()
-           ys, xs = torch.where(mask)
-           pred_boxes[i] = torch.tensor([xs.min(), ys.min(), xs.max(), ys.max()]).float()
-        results.pred_boxes = Boxes(pred_boxes)        
+        # pred_boxes = torch.zeros(seg_masks.size(0), 4)
+        # for i in range(seg_masks.size(0)):
+        #    mask = seg_masks[i].squeeze()
+        #    ys, xs = torch.where(mask)
+        #    pred_boxes[i] = torch.tensor([xs.min(), ys.min(), xs.max(), ys.max()]).float()
+        # results.pred_boxes = Boxes(pred_boxes)
 
         return results
 
